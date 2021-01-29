b120fc92e9941839bb1feb1d48f06ed7a9d0c36a
Greg Samuelson <grssam@gmail.com>
2021-01-29 10:45:43 -0800

    prep for pulling from carlo

--- controllers/nodemailer.js
+++ controllers/nodemailer.js
@@ -1,5 +1,6 @@
 "use strict";
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
+const crypto = require('crypto')
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
